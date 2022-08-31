031ec127ef24a1111764047d9beb4ce72d2c2e47
Stefan Buhrmester <buhrmi@gmail.com>
2022-08-31 11:24:08 +0200

    gstatic

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-const API_SERVICE_URL = "http://fonts.googleapis.com";
+app.use('/fonts', createProxyMiddleware({
+    target: "http://fonts.googleapis.com",
+    changeOrigin: true,
+    pathRewrite: {'^/fonts' : ''},
+    selfHandleResponse: true,
+    onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+        const response = responseBuffer.toString('utf8'); // convert buffer to string
+        return response.replaceAll('http://fonts.gstatic.com', 'https://fonts.buhrmi.de/gstatic'); // manipulate response and return the result
+    }),
+}))
 
-app.use('/', createProxyMiddleware({
-    target: API_SERVICE_URL,
-    changeOrigin: true
-}));
+app.use('/gstatic', createProxyMiddleware({
+    target: "http://fonts.gstatic.com",
+    changeOrigin: true,
+    pathRewrite: {'^/gstatic' : ''}
+}))
 
 app.listen(PORT, () => {
     console.log(`Proxy server listening at port number:${PORT}`);
-});
+})
